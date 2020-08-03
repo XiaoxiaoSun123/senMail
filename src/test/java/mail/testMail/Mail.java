b04e911e77250f430c39ed4bd0abd060478585b7
hxm154 <hxm154@sina.com>
2020-08-03 18:41:29 +0800

    更新

--- src/test/java/mail/testMail/Mail.java
+++ src/test/java/mail/testMail/Mail.java
@@ -88,7 +88,7 @@ public class Mail {
     
 @Test
 public void sendMail() throws UnsupportedEncodingException{
-	Mail.sendEmailAttach("古诗词宝巡检邮件--day2020","请查看报告有问题随时联系--胡晓明", "E:\\test_jmeter\\jmeter-jmx\\test_res\\plus_medplus-test-results.html");
+	Mail.sendEmailAttach("古诗词宝接口测试报告","报告请查看附件，有问题随时联系--胡晓明", "E:\\test_jmeter\\jmeter-jmx\\test_res\\plus_medplus-test-results.html");
 }
 //    public static void main(String[] args) throws IOException {
 ////            Mail.email_send("app","df df",);
